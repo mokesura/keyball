67e3f78d9a8f2491375a14609f1c7de1eac2041a
Chinatsu Chiba <47180063+mokesura@users.noreply.github.com>
2024-06-15 23:35:55 +0900

    mousekes on（意味ないかも）

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via_mokesura/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via_mokesura/keymap.c
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_ESC   , _______  , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_MUTE  , KC_VOLD  , KC_VOLU  , KC_CALC  , KC_F11   , KC_DEL   ,
     KC_TAB  ,GUI_T(KC_Q), KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , LT(4,KC_P),KC_BSPC  ,
-    KC_RSFT  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     ,LT(1,KC_J), KC_K     , KC_L     , KC_MINS  , KC_ENT   ,
+    KC_RSFT  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     ,LT(3,KC_J), KC_K     , KC_L     , KC_MINS  , KC_ENT   ,
     KC_LCTL  , KC_Z    ,ALT_T(KC_X), KC_C     , KC_V     , KC_B     , _______  ,            _______  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-    MO(3)    , KC_LGUI  , XXXXXXX , XXXXXXX , XXXXXXX ,LT(2,KC_LNG2),CTL_T(KC_SPC),  SFT_T(KC_ENT),LT(3,KC_LNG1), KC_DEL   , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT
+    MO(1)    , KC_LGUI  , XXXXXXX , XXXXXXX , XXXXXXX ,LT(2,KC_LNG2),CTL_T(KC_SPC),  SFT_T(KC_ENT),LT(3,KC_LNG1), KC_DEL   , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT
   ),
 
   [1] = LAYOUT_universal( // Mouse
